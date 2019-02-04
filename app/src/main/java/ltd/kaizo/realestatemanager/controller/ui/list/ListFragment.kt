1801d7d348d0f4fe917683fca462ffdb0a36a1ef
Kaizo <yanovich007@yahoo.fr>
2019-02-04 18:38:28 +0100

    add listActivity

---
+++ app/src/main/java/ltd/kaizo/realestatemanager/controller/ui/list/ListFragment.kt
@@ -0,0 +1,32 @@
+package ltd.kaizo.realestatemanager.controller.ui.list
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ltd.kaizo.realestatemanager.R
+
+class ListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ListFragment()
+    }
+
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
