aa670c7f7049450e54e725ed84e7c7ffe6886f67
Kaizo <yanovich007@yahoo.fr>
2019-04-01 16:14:04 +0200

    add mapActivity, location permission and mapApi dependency

---
+++ app/src/main/java/ltd/kaizo/realestatemanager/controller/ui/map/MapActivity.kt
@@ -0,0 +1,70 @@
+package ltd.kaizo.realestatemanager.controller.ui.map
+
+import android.content.Context
+import android.location.Location
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.livinglifetechway.quickpermissions_kotlin.runWithPermissions
+import kotlinx.android.synthetic.main.activity_map.*
+import ltd.kaizo.realestatemanager.R
+import ltd.kaizo.realestatemanager.controller.ui.base.BaseActivity
+import ltd.kaizo.realestatemanager.utils.RC_ERROR_DIALOG_REQUEST
+import ltd.kaizo.realestatemanager.utils.Utils.showSnackBar
+import timber.log.Timber
+
+class MapActivity : BaseActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var currentLocation: Location
+
+
+    override fun getFragmentLayout(): Int {
+        return R.layout.activity_map
+    }
+
+    override fun configureDesign() {
+        this.getCurrentLocation()
+    }
+
+    private fun getCurrentLocation() {
+        if (isServiceOK()) {
+            runWithPermissions(Context.LOCATION_SERVICE) {
+                this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                try {
+                    val location = this.fusedLocationProviderClient.lastLocation
+                    location.addOnCompleteListener { task ->
+                        if (task.isSuccessful && task.result != null) {
+                            currentLocation = task.result!!
+                            //TODO launch mapfragment 
+                        } else {
+                            showSnackBar(activity_map_coordinator_layout, getString(R.string.unable_get_location))
+                        }
+                    }
+                } catch (e: SecurityException) {
+                    Timber.e("security exception : %s", e.message)
+                    showSnackBar(activity_map_coordinator_layout, getString(R.string.unable_get_location))
+                }
+            }
+        }
+    }
+
+    /**
+     * check if the Google Play services are available to make map request
+     *
+     * @return Boolean boolean
+     */
+    private fun isServiceOK(): Boolean {
+        val available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        if (available == ConnectionResult.SUCCESS) {
+            return true
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            val dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, available, RC_ERROR_DIALOG_REQUEST)
+        } else {
+            showSnackBar(activity_map_coordinator_layout, getString(R.string.map_request_error))
+        }
+        return false
+    }
+
+
+}
--- app/src/main/java/ltd/kaizo/realestatemanager/utils/constant.kt
+++ app/src/main/java/ltd/kaizo/realestatemanager/utils/constant.kt
@@ -1,16 +1,28 @@
 package ltd.kaizo.realestatemanager.utils
 
-import android.Manifest.permission.CAMERA
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.Manifest.permission.*
 
-
+/****************************
+ *********     APP    ********
+ *****************************/
 const val DATABASE_NAME = "ESTATE_DB"
 const val ESTATE_SOURCE = "ESTATE SOURCE"
 const val ESTATE_ID = "ESTATE ID"
 const val ESTATE_SOURCE_ADD = 40
 const val ESTATE_SOURCE_EDIT = 30
+const val TAG_DIALOG = "AddPictureDialogFragment"
+const val COLLECTION_NAME = "users"
+
+/****************************
+ *********   PERMS   ********
+ *****************************/
 const val WRITE_EXT_PERM = WRITE_EXTERNAL_STORAGE
 const val CAMERA_PERM = CAMERA
+const val FINE_LOCATION = ACCESS_FINE_LOCATION
+
+/****************************
+ ****   RESPONSE CODE   ******
+ *****************************/
 const val RC_TAKE_PHOTO = 100
 const val RC_CHOOSE_PHOTO = 200
 const val RC_DATE_OUT = 300
@@ -20,5 +32,5 @@ const val RC_PICTURE_ITEM_EDIT = 600
 const val RC_PICTURE_LISTENER_REMOVE = 700
 const val RC_PICTURE_LISTENER_VIEW = 800
 const val RC_PICTURE_LISTENER_FAVORITE = 900
-const val TAG_DIALOG = "AddPictureDialogFragment"
-const val COLLECTION_NAME = "users"
+const val RC_ERROR_DIALOG_REQUEST = 9001
+const val RC_LOCATION_PERMISSION = 1234
